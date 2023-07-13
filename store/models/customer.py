d9d32a0aba3062fa9446570e1717c3b9cec541ba
vais2001 <vaishali.ongraph@gmail.com>
2023-07-13 18:50:24 +0530

    updation4

--- store/models/customer.py
+++ store/models/customer.py
@@ -5,23 +5,23 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
-    email = models.EmailField(max_length=200)
+    email = models.EmailField()
     password = models.CharField(max_length=500)
 
-    # def register(self):
-    #     self.save()
+    def register(self):
+        self.save()
 
-    # @staticmethod
-    # def get_customer_by_email(email):
-    #     try:
-    #         return Customer.objects.get(email=email)
-    #     except:
-    #         return False
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
 
+# for unique mail
+    def isExists(self):
+        if Customer.objects.get(email = self.email):
+            return True
 
-    # def isExists(self):
-    #     if Customer.objects.filter(email = self.email):
-    #         return True
-
-    #     return  False
+        return  False
 
--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import index 
 from .views import signup
+from .views import login
 urlpatterns =[
-    path('',index)
-]
-urlpatterns=[
-    path('signup',signup)
+    path('',index,name='homepage'),
+    path('signup/',signup,name='signup'),
+    path('login',login ,name='loginpage')
 ]
